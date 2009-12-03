316b95d937daec11965cc1024a567321a50bec9e
ianlesnet <ianlesnet@6dd3200a-a398-11de-8a4e-6d34bb13bdfc>
2009-12-03 08:41:48 +0000

    cleanup

--- source/STK500v2/base.h
+++ source/STK500v2/base.h
@@ -26,7 +26,7 @@
 
 //these compile time options enable various features
 #define ENABLE_VREG 			//voltage regulators always on
-#define OUTPUT_HIGH_IMPEDANCE	//use high impedance outputs for multi-voltage interfacing
+//#define OUTPUT_HIGH_IMPEDANCE	//use high impedance outputs for multi-voltage interfacing
 #ifdef OUTPUT_HIGH_IMPEDANCE	
 	#define ENABLE_PULLUP_RESISTORS //enable on-board pullup resistors (only if Hi-Z, only on hardware v2, v3)
 #endif
