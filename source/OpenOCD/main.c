91989fb9860432fcc6b096ecd9eb4bbe137b10a4
ianlesnet <ianlesnet@6dd3200a-a398-11de-8a4e-6d34bb13bdfc>
2010-01-04 13:29:54 +0000

    initial openocd port

---
+++ source/OpenOCD/main.c
@@ -0,0 +1,201 @@
+/*
+ * usbprog - A Downloader/Uploader for AVR device programmers
+ * Copyright (C) 2006 Benedikt Sauter 
+    //USBNDeviceManufacture("B.Sauter");
+    //USBNDeviceProduct("OpenOCD Debugger");
+    //USBNDeviceSerialNumber("GNU/GPL2");
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#define UNKOWN_COMMAND	0x00
+#define PORT_DIRECTION	0x01
+#define PORT_SET	0x02
+#define PORT_GET	0x03
+#define PORT_SETBIT	0x04
+#define PORT_GETBIT	0x05
+#define WRITE_TDI	0x06
+#define READ_TDO	0x07
+#define WRITE_AND_READ	0x08
+#define WRITE_TMS	0x09
+#define WRITE_TMS_CHAIN	0x0A
+
+//#define F_CPU 16000000
+#include "base.h"
+#include "UART.h"
+#include "usbprogjtag.h"
+
+//set custom configuration for PIC 24F
+_CONFIG2(FNOSC_FRCPLL & OSCIOFNC_ON &POSCMOD_NONE & I2C1SEL_PRI)		// Internal FRC OSC = 8MHz
+_CONFIG1(JTAGEN_OFF & GCP_OFF & GWRP_OFF & COE_OFF & FWDTEN_OFF & ICS_PGx1) //turn off junk we don't need
+unsigned int userReset  __attribute__ ((space(prog),section(".init"))) = 0xC04 ;//bootloader stuff
+unsigned char timeout  __attribute__ ((space(prog),section(".init"))) = 0x00 ;
+
+void CommandAnswer(int length);
+void Commands(void);
+
+char answer[64];
+char buf[1024];//max packet should be 320 bytes, but why not 1024?
+
+void CommandAnswer(int length){
+  int i;
+
+  for(i = 0; i < length; i++)
+    UART1TX(answer[i]);
+}
+
+/* central command parser */
+void Commands(void){
+  int i;
+  switch(buf[0]){
+    case PORT_DIRECTION://1 extra byte
+      set_direction((uint8_t)buf[1]);
+    break;
+    case PORT_SET://1 extra byte
+      set_port((uint8_t)buf[1]);
+    break;
+    case PORT_GET://0 extra bytes
+      answer[0] = PORT_GET; 
+      answer[1] = get_port();
+      CommandAnswer(2);
+    break;
+    case PORT_SETBIT://2 extra bytes
+      set_bit((uint8_t)buf[1],(uint8_t)buf[2]);
+    break;
+    case PORT_GETBIT://1 extra bytes
+      answer[0] = PORT_GETBIT; 
+      answer[1] = (char)get_bit((uint8_t)buf[1]);
+      CommandAnswer(2);
+    break;
+    
+    case WRITE_TDI:// buf[1]+buf[2] extra bytes?????
+      write_tdi(buf,((uint8_t)buf[1]*256)+(uint8_t)buf[2]);	// size = numbers of byte not bits!!! round up
+      // tck 0 tms 0 tdi 0
+      CLEARBIT(BIT2_WRITE,BIT2);  // clk
+      CLEARBIT(BIT1_WRITE,BIT1);  // tdi
+      CLEARBIT(BIT3_WRITE,BIT3);  // tms
+      
+      // tck 1 tms 0 tdi 0
+      SETBIT(BIT2_WRITE,BIT2);  // clk
+    break;
+ 
+    case WRITE_TMS://1 extra bytes
+      write_tms((uint8_t)buf[1]);
+    break;
+   
+    case WRITE_TMS_CHAIN://buf[1] extra bytes
+      for(i=0;i<(int)buf[1];i++){
+		write_tms((uint8_t)buf[2+i]);
+		asm("nop");
+		asm("nop");
+      }
+    break;
+
+    case READ_TDO:// buf[1]+buf[2] extra bytes?????
+      read_tdo(buf,((uint8_t)buf[1]*256)+(uint8_t)buf[2]);	// size = numbers of byte not bits!!! round up
+      #if 1
+      // tck 0 tms 0 tdi 0
+      CLEARBIT(BIT2_WRITE,BIT2);  // clk
+      CLEARBIT(BIT1_WRITE,BIT1);  // tdi
+      CLEARBIT(BIT3_WRITE,BIT3);  // tms
+      
+      // tck 1 tms 0 tdi 0
+      SETBIT(BIT2_WRITE,BIT2);  // clk
+      #endif
+      for(i=0;i<64;i++)
+		answer[i]=buf[i];
+      CommandAnswer(64);
+    break;
+
+    case WRITE_AND_READ:// buf[1]+buf[2] extra bytes?????
+      write_and_read(buf,((uint8_t)buf[1]*256)+(uint8_t)buf[2]);	// size = numbers of byte not bits!!! round up
+      
+      #if 1
+      // tck 0 tms 0 tdi 0
+      CLEARBIT(BIT2_WRITE,BIT2);  // clk
+      CLEARBIT(BIT1_WRITE,BIT1);  // tdi
+      CLEARBIT(BIT3_WRITE,BIT3);  // tms
+      
+      // tck 1 tms 0 tdi 0
+      SETBIT(BIT2_WRITE,BIT2);  // clk
+      #endif
+      for(i=0;i<64;i++)
+		answer[i]=buf[i];
+      CommandAnswer(64);
+    break;
+    
+    default:// 0 extra bytes
+      // unkown command
+      answer[0] = UNKOWN_COMMAND; 
+      answer[1] = 0x00; 
+      CommandAnswer(2);
+  }
+
+
+}
+
+
+//this is adopted from USB bulk to UART serial on the Bus Pirate
+//		since we don't have packet transfer abilities, 
+//      each case gathers the extra bytes it needs
+//		before handing off processing to the Commands function
+int main(void){
+	int i,j;
+
+	//do setup and configuration
+	Initialize(); 
+	InitializeUART1();
+	BP_LEDMODE_DIR=0; //LED to output
+	BP_LEDMODE=1;//mode LED on
+
+    while(1){
+		buf[0]=UART1RX();//get command byte
+  		switch(buf[0]){
+    		case PORT_DIRECTION://1 extra byte
+    		case PORT_SET://1 extra byte
+    		case PORT_GETBIT://1 extra bytes
+    		case WRITE_TMS://1 extra bytes
+				buf[1]=UART1RX();//get extra byte
+				break;
+    		case PORT_SETBIT://2 extra bytes
+				buf[1]=UART1RX();//get extra byte
+				buf[2]=UART1RX();//get extra byte
+				break;
+    		case WRITE_TMS_CHAIN://buf[1] extra bytes    
+				buf[1]=UART1RX();//get data length
+			   	for(i=0;i<(int)buf[1];i++){
+					buf[2+i]=UART1RX();
+				}
+				break;
+    		case WRITE_TDI:// buf[1]+buf[2] extra bytes?????
+			case READ_TDO:// buf[1]+buf[2] extra bytes?????
+    		case WRITE_AND_READ:// buf[1]+buf[2] extra bytes?????
+				buf[1]=UART1RX();//get extra byte
+				buf[2]=UART1RX();//get extra byte
+				j=((uint8_t)buf[1]*256)+(uint8_t)buf[2]; //get data packet size
+			   	for(i=0;i<j;i++){
+					buf[2+i]=UART1RX();
+				}
+				break;
+    		case PORT_GET://0 extra bytes
+			default:// 0 extra bytes
+				break;
+  		}
+		Commands();//process first command byte, gather remaining bytes wihtin this function
+	}
+
+}
+
+
---
+++ source/OpenOCD/usbprogjtag.h
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2006 Benedikt Sauter 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include "base.h"
+#define BIT(x) (x)
+#define SETBIT(addr,x)    (addr |= x)
+#define CLEARBIT(addr,x)  (addr &= ~(x))
+
+#define BIT0	    MISO //tdo(?)
+#define BIT1  	    MOSI //tdi
+#define BIT2	    CLK //clk
+#define BIT3	    CS //tms
+
+#define BIT4	    0b1//jp2left(?)
+#define BIT5	    0b01//jp2right(?)
+
+#define BIT4_1	    0b1//jumper power???
+#define BIT5_1	    0b01
+
+#define BIT6	    LEDMODE //red LED
+
+
+#define BIT0_DDR    IODIR
+#define BIT1_DDR    IODIR
+#define BIT2_DDR    IODIR
+#define BIT3_DDR    IODIR
+
+#define BIT4_DDR    IODIR
+#define BIT4_1_DDR  IODIR 
+#define BIT5_DDR    IODIR
+#define BIT5_1_DDR  IODIR  
+#define BIT6_DDR    LEDMODEDIR
+
+
+#define BIT0_READ   IOPOR
+#define BIT1_READ   IOPOR
+#define BIT2_READ   IOPOR
+#define BIT3_READ   IOPOR
+
+#define BIT4_READ   IOPOR
+#define BIT5_READ   IOPOR
+#define BIT4_1_READ  IOPOR 
+#define BIT5_1_READ  IOPOR 
+#define BIT6_READ   LEDMODEPOR
+
+//output port for pin writes (see3 hardwarevX.h for actual pins)
+#define BIT0_WRITE   IOLAT
+#define BIT1_WRITE   IOLAT
+#define BIT2_WRITE   IOLAT
+#define BIT3_WRITE   IOLAT
+
+#define BIT4_WRITE   IOLAT
+#define BIT5_WRITE   IOLAT
+#define BIT4_1_WRITE  IOLAT 
+#define BIT5_1_WRITE  IOLAT 
+#define BIT6_WRITE   LEDMODELAT
+
+// check if tdo == 1
+#define IS_BIT0_SET()			    (BIT0_READ & BIT(BIT0))
+#define IS_BIT1_SET()			    (BIT1_READ & BIT(BIT1)) 
+#define IS_BIT2_SET()			    (BIT2_READ & BIT(BIT2))
+#define IS_BIT3_SET()			    (BIT3_READ & BIT(BIT3))
+
+#define IS_BIT4_SET()			    (BIT4_READ & BIT(BIT4))
+#define IS_BIT5_SET()			    (BIT5_READ & BIT(BIT5))
+#define IS_BIT4_1_SET()			    (BIT4_1_READ & BIT(BIT4_1))
+#define IS_BIT5_1_SET()			    (BIT5_1_READ & BIT(BIT5_1))
+#define IS_BIT6_SET()			    (BIT6_READ & BIT(BIT6))
+
+
+#define SET_BIT0()			     SETBIT( BIT0_WRITE, BIT0 )
+#define SET_BIT1()			     SETBIT( BIT1_WRITE, BIT1 )
+#define SET_BIT2()			     SETBIT( BIT2_WRITE, BIT2 )
+#define SET_BIT3()			     SETBIT( BIT3_WRITE, BIT3 )
+
+#define SET_BIT4()			     SETBIT( BIT4_WRITE, BIT4 )
+#define SET_BIT5()			     SETBIT( BIT5_WRITE, BIT5 )
+#define SET_BIT4_1()			     SETBIT( BIT4_1_WRITE, BIT4_1 )
+#define SET_BIT5_1()			     SETBIT( BIT5_1_WRITE, BIT5_1 )
+#define SET_BIT6()			     SETBIT( BIT6_WRITE, BIT6 )
+
+#define CLEAR_BIT0()			     CLEARBIT( BIT0_WRITE, BIT0 )
+#define CLEAR_BIT1()			     CLEARBIT( BIT1_WRITE, BIT1 )
+#define CLEAR_BIT2()			     CLEARBIT( BIT2_WRITE, BIT2 )
+#define CLEAR_BIT3()			     CLEARBIT( BIT3_WRITE, BIT3 )
+
+#define CLEAR_BIT4()			     CLEARBIT( BIT4_WRITE, BIT4 )
+#define CLEAR_BIT5()			     CLEARBIT( BIT5_WRITE, BIT5 )
+#define CLEAR_BIT4_1()			     CLEARBIT( BIT4_1_WRITE, BIT4_1 )
+#define CLEAR_BIT5_1()			     CLEARBIT( BIT5_1_WRITE, BIT5_1 )
+#define CLEAR_BIT6()			     CLEARBIT( BIT6_WRITE, BIT6 )
+
+
+
+
+
+void write_tdi(char *buf, uint16_t size);
+void write_tms(uint8_t  buf);
+void write_and_read(char * buf, uint16_t size);
+void read_tdo(char * buf, uint16_t size);
+
+void set_direction(uint8_t direction);
+void set_port(uint8_t value);
+uint8_t get_port();
+void set_bit(uint8_t bit, uint8_t value);
+uint8_t get_bit(uint8_t bit);
+
