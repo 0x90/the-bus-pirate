c9835c797d0047a882f74829f06ab340273cb2cb
ianlesnet <ianlesnet@6dd3200a-a398-11de-8a4e-6d34bb13bdfc>
2010-08-19 16:49:37 +0000

    BPv4 USB test firmware

---
+++ source-v4/HardwareProfile.h
@@ -0,0 +1,13 @@
+#ifndef HARDWARE_PROFILE_H
+#define HARDWARE_PROFILE_H
+#include <p24fxxxx.h>
+//these are unneeded, but the stack complains if they're not defined
+//#define USE_SELF_POWER_SENSE_IO
+#define tris_self_power     TRISCbits.TRISC2    
+#define self_power          1
+
+//#define USE_USB_BUS_SENSE_IO
+#define tris_usb_bus_sense  TRISCbits.TRISC2    
+#define USB_BUS_SENSE       1 
+
+#endif 
