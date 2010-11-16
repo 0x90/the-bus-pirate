3e58c3e12a1003aa8a35258e4914f7b859ea15c7
yobibe <yobibe@6dd3200a-a398-11de-8a4e-6d34bb13bdfc>
2010-11-16 14:23:11 +0000

    New MPLABX project directory, see status in full commit msg
    ADDONS firmware with BUSPIRATEV3: OK
    MAIN   firmware:                  too big
    ADDONS firmware with BUSPIRATEV4: FAIL

---
+++ source/busPirate.X/busPirate/nbproject/Makefile-default.mk
@@ -0,0 +1,98 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Include project Makefile
+include Makefile
+
+# Environment
+MKDIR=mkdir -p
+RM=rm -f 
+CP=cp 
+# Macros
+CND_CONF=default
+
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+IMAGE_TYPE=debug
+FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/busPirate.${IMAGE_TYPE}.elf
+else
+IMAGE_TYPE=production
+FINAL_IMAGE=dist/${CND_CONF}/${IMAGE_TYPE}/busPirate.${IMAGE_TYPE}.elf
+endif
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
+# Distribution Directory
+DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
+
+# Object Files
+OBJECTFILES=
+
+
+CFLAGS=
+ASFLAGS=
+LDLIBSOPTIONS=
+
+OS_ORIGINAL="Linux"
+OS_CURRENT="$(shell uname -s)"
+############# Tool locations ##########################################
+# If you copy a project from one host to another, the path where the  #
+# compiler is installed may be different.                             #
+# If you open this project with MPLAB X in the new host, this         #
+# makefile will be regenerated and the paths will be corrected.       #
+#######################################################################
+MP_CC=/opt/microchip/mplabc30/v3.24/bin/pic30-gcc
+MP_AS=/opt/microchip/mplabc30/v3.24/bin/pic30-as
+MP_LD=/opt/microchip/mplabc30/v3.24/bin/pic30-ld
+MP_AR=/opt/microchip/mplabc30/v3.24/bin/pic30-ar
+MP_CC_DIR=/opt/microchip/mplabc30/v3.24/bin
+MP_AS_DIR=/opt/microchip/mplabc30/v3.24/bin
+MP_LD_DIR=/opt/microchip/mplabc30/v3.24/bin
+MP_AR_DIR=/opt/microchip/mplabc30/v3.24/bin
+.build-conf: ${BUILD_SUBPROJECTS}
+ifneq ($(OS_CURRENT),$(OS_ORIGINAL))
+	@echo "***** WARNING: This make file contains OS dependent code. The OS this makefile is being run is different from the OS it was created in."
+endif
+	${MAKE}  -f nbproject/Makefile-default.mk dist/${CND_CONF}/${IMAGE_TYPE}/busPirate.${IMAGE_TYPE}.elf
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: assemble
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: compile
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+else
+endif
+
+# ------------------------------------------------------------------------------------
+# Rules for buildStep: link
+ifeq ($(TYPE_IMAGE), DEBUG_RUN)
+dist/${CND_CONF}/${IMAGE_TYPE}/busPirate.${IMAGE_TYPE}.elf: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_CC}  -omf=elf  -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -o dist/${CND_CONF}/${IMAGE_TYPE}/busPirate.${IMAGE_TYPE}.elf ${OBJECTFILES}      -Wl,--defsym=__MPLAB_BUILD=1,-Tp24FJ64GA002.gld,--defsym=__MPLAB_DEBUG=1,--defsym=__ICD2RAM=1,--defsym=__DEBUG=1,--defsym=__MPLAB_DEBUGGER_PK3=1
+else
+dist/${CND_CONF}/${IMAGE_TYPE}/busPirate.${IMAGE_TYPE}.elf: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk
+	${MKDIR} dist/${CND_CONF}/${IMAGE_TYPE} 
+	${MP_CC}  -omf=elf  -o dist/${CND_CONF}/${IMAGE_TYPE}/busPirate.${IMAGE_TYPE}.elf ${OBJECTFILES}      -Wl,--defsym=__MPLAB_BUILD=1,-Tp24FJ64GA002.gld
+	${MP_CC_DIR}/pic30-bin2hex dist/${CND_CONF}/${IMAGE_TYPE}/busPirate.${IMAGE_TYPE}.elf -omf=elf
+endif
+
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf:
+	${RM} -r build/default
+	${RM} -r dist
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
